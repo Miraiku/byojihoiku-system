843983fa8bfb1c38709c891c82883112d88b7267
MinamiYODA <me@usaribbon.net>
2021-12-08 19:55:07 +0900

    line register

--- index.js
+++ index.js
@@ -383,6 +383,8 @@ function isZenkakuKana(s) {
 }
 
 function isBirthdayNum(s){
+  date = new Date(s).substr( 0, 4 ), str.substr( 4, 2 ), str.substr( 6, 2 ));   
+  console.log('birthday:'+ date); 
   if(s.match(/^[0-9]+$/) && s.length==8){
     return true
   }else{
