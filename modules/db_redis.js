665ed7f7454a2c0662098f849295863aea1882ea
MinamiYODA <me@usaribbon.net>
2021-12-15 16:03:44 +0900

    add cron delete all redis per hour

--- modules/db_redis.js
+++ modules/db_redis.js
@@ -7,10 +7,12 @@ const redis_modules = require('./db_redis')
 
 exports.hsetStatus = async function (id,key,val){
   try {
-    await redis_client.hset('update_time',id,Date.now(), (err, reply) => {
+    let now = Date.now()
+    await redis_client.hset('update_time',id, now, (err, reply) => {
       if (err) throw err;
-      console.log('HSET updated time : id:' + key + ', time: '+ val);
+      console.log('HSET updated time : id:' + id + ', time: '+ now);
     });
+    redis_modules.flushALLNoUpdate20mins()
     await redis_client.hset(id,key,val, (err, reply) => {
       if (err) throw err;
       console.log('HSET Status :'+ id + ', key:' + key + ', val: '+ val);
@@ -79,11 +81,21 @@ exports.flushALLNoUpdate20mins = async function(){
     let result
     await redis_client.hgetall('update_time', (err, reply) => {
       if (err) throw err;
-      console.log('HGETALL No Update within 20mins :'+ reply);
+      console.log('HDETALL No Update within 20mins :'+ reply);
       result = reply
     });
     Object.entries(result).forEach(([k, v]) => {
-      redis_modules.resetAllStatus(k, v)
+      let now = Date.now()
+      let diff_time = now - v;
+      console.log("DIFF TIMEL " +diff_time)
+      let pass_seconds = Math.floor(diff_time / 1000);
+      console.log(pass_seconds + "秒が経過");
+      // 644428213秒が経過
+
+      var pass_minutes = Math.floor(diff_time / (60 * 1000));
+      if(now < v){
+        redis_modules.resetAllStatus(k)
+      }
     });
   }
   catch (err) {
