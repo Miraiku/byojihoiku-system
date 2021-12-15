85df0767ab3bc84c0045544a7cb440c6889370ab
MinamiYODA <me@usaribbon.net>
2021-12-15 17:11:25 +0900

    nursery

--- modules/db_postgre.js
+++ modules/db_postgre.js
@@ -33,7 +33,7 @@ exports.getNurseryTable = async function (){
 exports.getAvailableNurseryOnThatDay = async function (date){
   let available = {}
   Object.entries(await psgl.getNurseryTable()).forEach(async ([k, v]) =>  {
-      let sql = `SELECT COUNT ("NurseryID") FROM public."Reservation" WHERE "ReservationStatus" = 'Reserved' and "ReservationDate"::text LIKE '`+date+`%' and "NurseryID" = '`+v['ID']+`%';`
+      let sql = `SELECT COUNT ("ID") FROM public."Reservation" WHERE "ReservationStatus" = 'Reserved' and "ReservationDate"::text LIKE '`+date+`%' and "NurseryID" = '`+v['ID']+`';`
       let c = await psgl.sqlToPostgre(sql)
       if(Number(c) <= Number(v['Capacity'])){
         available += {id: v['ID'], capacity: c}
