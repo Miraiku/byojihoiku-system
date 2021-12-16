458c71d4a2aafc85ab65792db84b8e330efee154
MinamiYODA <me@usaribbon.net>
2021-12-16 16:57:03 +0900

    working async iterator

--- modules/line.js
+++ modules/line.js
@@ -171,7 +171,7 @@ router
                       }
                   });
   
-                  replyMessage = "希望日は「"+DayToJP(text)+getDayString(dayaftertomorrow)+"」ですね。\n希望利用の園を以下から選択してください。\n"+all_info
+                  replyMessage = "希望日は「"+DayToJP(text)+getDayString(text)+"」ですね。\n希望利用の園を以下から選択してください。\n"+all_info
                   //redis.hsetStatus(userId,'reservation_date',text)
                   //redis.hsetStatus(userId,'reservation_status',2)
                   //redis.hsetStatus(userId,'reservation_reply_status',20)
