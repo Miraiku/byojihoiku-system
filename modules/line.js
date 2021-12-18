7ab5bb381180175058c7d16976d838e47df5361a
MinamiYODA <me@usaribbon.net>
2021-12-18 17:36:03 +0900

    step 14-16

--- modules/line.js
+++ modules/line.js
@@ -231,11 +231,11 @@ router
                 let first_nursery = await redis.hgetStatus(userId, 'reservation_nursery_name_1')
                 let open = await redis.hgetStatus(userId, 'reservation_nursery_opentime')
                 let close = await redis.hgetStatus(userId, 'reservation_nursery_closetime')
-                let nursery_id = await getNurseryIdByName(text)
                 replyMessage = "利用希望の園は「"+text+"」ですね。\n登園時間を返信してください。\n\n"+first_nursery+"の開園時間は、"+open+"〜"+close+"です。\n例）9時に登園する場合は「0900」"
                 redis.hsetStatus(userId,'reservation_nursery_name_3',text)
                 if( text == 'なし'){
-                  redis.hsetStatus(userId,'reservation_nursery_id_3',0)
+                  console.log(text)
+                  await redis.hsetStatus(userId,'reservation_nursery_id_3',0)
                 }
                 redis.hsetStatus(userId,'reservation_status',5)
                 redis.hsetStatus(userId,'reservation_reply_status',50)
