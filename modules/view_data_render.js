92a085ab554e045a3a926e5f8dbb44c1c0c6e991
MinamiYODA <me@usaribbon.net>
2021-12-29 00:12:06 +0900

    member

--- modules/view_data_render.js
+++ modules/view_data_render.js
@@ -202,7 +202,7 @@ exports.getEntryPage = async function (req, res){
     let age = view.getAgeMonth(info[0].BirthDay)
     let allergy = info[0].Allergy
     mem.push({miraikuid:id, name:name, birthday:birthday, bYear:bYear, bMonth:bMonth, bDay:bDay, age:age, allergy:allergy, memberid:info[0].ID})
-    console.log(Member)
+    console.log(mem)
     res.render("pages/member/entry",{Member:mem})
   } catch (error) {
     console.log("ERR @getEntryPage: "+ error)
