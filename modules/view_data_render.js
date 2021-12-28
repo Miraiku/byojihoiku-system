3567ab3843ccf35c90caeb44b5a171075d71afce
MinamiYODA <me@usaribbon.net>
2021-12-28 15:44:04 +0900

    view - data

--- modules/view_data_render.js
+++ modules/view_data_render.js
@@ -15,9 +15,11 @@ exports.getMemberNameByMemberID = async function (req, res){
     const nursery_list = await psgl.getNurseryID_Name_Capacity();
     let status3days = []
     for(let i = 0; i < nursery_list.length; i++){
-      status3days.push({id:nursery_list[i].id, name:nursery_list[i].name, status:await psgl.ReservationStatus3DaysByNursery(nursery_list[i].id)})
+      status3days.push(await psgl.ReservationStatus3DaysByNursery(nursery_list[i].id))
+      console.log(status3days[i])
+      //status3days.push({id:nursery_list[i].id, name:nursery_list[i].name, status:})
     }
-    console.log(status3days)
+    
     if (error) {
       throw error;
   }
