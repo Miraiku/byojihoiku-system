ff96905ac6ba5ea6380cbdec98c7f94b379f05c4
MinamiYODA <me@usaribbon.net>
2021-12-29 09:47:09 +0900

    reservation

--- modules/view_data_render.js
+++ modules/view_data_render.js
@@ -310,11 +310,98 @@ exports.getCalendarPage = async function (req, res){
 exports.getReservationPage = async function (req, res){
   try {
     const nurseryid = req.params.nurseryid
-    console.log(nurseryid)
+    console.log(typeof nurseryid)
     if(!view.isValidNum(nurseryid)){
+      console.log('invalid num')
       throw new Error('invalid num')
     }
-    res.render("pages/reservation/index")
+
+    /*　各園の3日間の状況 */
+    const JST = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })
+    const today_JST = new Date(JST)
+    const tomorrow_JST = new Date(today_JST);
+    tomorrow_JST.setDate(tomorrow_JST.getDate() + 1);
+    const dayaftertomorrow_JST = new Date(today_JST);
+    dayaftertomorrow_JST.setDate(dayaftertomorrow_JST.getDate() + 2);
+    let today_data, tomorrow_data, dayaftertomorrow_data
+
+    let status3days = []
+    const nursery_name = await psgl.getNurseryNameByID(nurseryid)
+    let todayStatus = await psgl.ReservationStatusTodayByNursery(nurseryid)
+    if(todayStatus.length > 0){
+      for (const status of todayStatus) {
+        let Unread = 0
+        let Cancelled = 0
+        let Waiting = 0
+        let Rejected = 0
+        let Reserved = 0
+        if(status.ReservationStatus == 'Unread'){
+          Unread += 1
+        }else if(status.ReservationStatus == 'Cancelled'){
+          Cancelled += 1
+        }else if(status.ReservationStatus == 'Waiting'){
+          Waiting += 1
+        }else if(status.ReservationStatus == 'Rejected'){
+          Rejected += 1
+        }else if(status.ReservationStatus == 'Reserved'){
+          Reserved += 1
+        }
+        today_data = {date:DayToJPFromDateObj(today_JST), unread:Unread, cancelled:Cancelled, waiting:Waiting, rejected:Rejected, reserved:Reserved}
+      }
+    }else{
+      today_data = {date:DayToJPFromDateObj(today_JST), unread:0, cancelled:0, waiting:0, rejected:0, reserved:0}
+    }
+    let tomorrowStatus = await psgl.ReservationStatusTomorrowByNursery(nurseryid)
+    if(tomorrowStatus.length > 0){
+      for (const status of await psgl.ReservationStatusTomorrowByNursery(nursery_list[i].id)) {
+        let Unread = 0
+        let Cancelled = 0
+        let Waiting = 0
+        let Rejected = 0
+        let Reserved = 0
+        if(status.ReservationStatus == 'Unread'){
+          Unread += 1
+        }else if(status.ReservationStatus == 'Cancelled'){
+          Cancelled += 1
+        }else if(status.ReservationStatus == 'Waiting'){
+          Waiting += 1
+        }else if(status.ReservationStatus == 'Rejected'){
+          Rejected += 1
+        }else if(status.ReservationStatus == 'Reserved'){
+          Reserved += 1
+        }
+        tomorrow_data = {date:DayToJPFromDateObj(tomorrow_JST), unread:Unread, cancelled:Cancelled, waiting:Waiting, rejected:Rejected, reserved:Reserved}
+      }
+    }else{
+      tomorrow_data = {date:DayToJPFromDateObj(tomorrow_JST), unread:0, cancelled:0, waiting:0, rejected:0, reserved:0}
+    }
+    let dayaftertomorrowStatus = await psgl.ReservationStatusDayAfterTomorrowByNursery(nurseryid)
+    if(dayaftertomorrowStatus.length > 0){
+      for (const status of await psgl.ReservationStatusDayAfterTomorrowByNursery(nursery_list[i].id)) {
+        let Unread = 0
+        let Cancelled = 0
+        let Waiting = 0
+        let Rejected = 0
+        let Reserved = 0
+        if(status.ReservationStatus == 'Unread'){
+          Unread += 1
+        }else if(status.ReservationStatus == 'Cancelled'){
+          Cancelled += 1
+        }else if(status.ReservationStatus == 'Waiting'){
+          Waiting += 1
+        }else if(status.ReservationStatus == 'Rejected'){
+          Rejected += 1
+        }else if(status.ReservationStatus == 'Reserved'){
+          Reserved += 1
+        }
+        dayaftertomorrow_data = {date:DayToJPFromDateObj(dayaftertomorrow_JST), unread:Unread, cancelled:Cancelled, waiting:Waiting, rejected:Rejected, reserved:Reserved}
+      }
+    }else{
+      dayaftertomorrow_data = {date:DayToJPFromDateObj(dayaftertomorrow_JST), unread:0, cancelled:0, waiting:0, rejected:0, reserved:0}
+    }
+    status3days.push({id:nurseryid, name:nursery_name[0].Name, today:today_data, tomorrow:tomorrow_data, dayaftertomorrow:dayaftertomorrow_data})
+    console.log(status3days)
+    res.render("pages/reservation/index", {Status3Days: status3days})
   } catch (error) {
     console.log("ERR @getReservationPage: "+ error)
     res.redirect('/home/')
