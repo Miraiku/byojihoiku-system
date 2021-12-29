c5942b9159a1b2fb8acaceb571ec42fbe8d84287
MinamiYODA <me@usaribbon.net>
2021-12-29 17:38:21 +0900

    confirm btn

--- public/js/functions.js
+++ public/js/functions.js
@@ -10,9 +10,8 @@ $(function() {
   $(".btn_status_update").on('click', function(e) {
     console.log(e);
     console.log($(this).val());
-    console.log($(this).name());
     e.preventDefault();              
-    $.confirmModal('「'+$(this).name()+'」に変更してよろしいですか？', function(el) {
+    $.confirmModal('「'+$(this).text()+'」に変更してよろしいですか？', function(el) {
       
     console.log(el);
       //do something...
