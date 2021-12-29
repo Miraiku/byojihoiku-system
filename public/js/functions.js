49a4151160588ef2aced194143ad0306f8339d44
MinamiYODA <me@usaribbon.net>
2021-12-29 17:21:28 +0900

    confirm btn

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,18 +1,10 @@
 const message = "変更を保存しますか？";
 const title = "確認メッセージ";
 
-console.log('aaaaaa')
-$(".btn_status_update").on('click', function() {
-  eModal
-  .confirm(message, title)
-  .then(
-    function (dom) { 
-
-      console.log('updated:'+dom)
-
-    },
-    function (dom) { 
-      console.log('updated cancel:'+dom)
-    }
-  );
+$(".btn_status_update").on('click', function(e) {
+  e.preventDefault();              
+  $.confirmModal('Are you sure you want to do this?', function(el) {
+    alert('OK was clicked!');
+    //do something...
+  });         
 });
