71bf0da145d324a5bb382e82cd978513ae40d7fb
MinamiYODA <me@usaribbon.net>
2021-12-29 11:32:12 +0900

    reservation

--- modules/view_data_render.js
+++ modules/view_data_render.js
@@ -323,84 +323,15 @@ exports.getReservationPage = async function (req, res){
     tomorrow_JST.setDate(tomorrow_JST.getDate() + 1);
     const dayaftertomorrow_JST = new Date(today_JST);
     dayaftertomorrow_JST.setDate(dayaftertomorrow_JST.getDate() + 2);
+    const nursery_name = await psgl.getNurseryNameByID(nurseryid)
+
     let today_data, tomorrow_data, dayaftertomorrow_data
+    today_data = {date:MonthDayToJPFromDateObj(today_JST),day:WeekDayToJPFromDateObj(today_JST)}
+    tomorrow_data = {date:MonthDayToJPFromDateObj(tomorrow_JST),day:WeekDayToJPFromDateObj(tomorrow_JST)}
+    dayaftertomorrow_data = {date:MonthDayToJPFromDateObj(dayaftertomorrow_JST),day:WeekDayToJPFromDateObj(dayaftertomorrow_JST)}
 
     let status3days = []
-    const nursery_name = await psgl.getNurseryNameByID(nurseryid)
-    let todayStatus = await psgl.ReservationStatusTodayByNursery(nurseryid)
-    if(todayStatus.length > 0){
-      for (const status of todayStatus) {
-        let Unread = 0
-        let Cancelled = 0
-        let Waiting = 0
-        let Rejected = 0
-        let Reserved = 0
-        if(status.ReservationStatus == 'Unread'){
-          Unread += 1
-        }else if(status.ReservationStatus == 'Cancelled'){
-          Cancelled += 1
-        }else if(status.ReservationStatus == 'Waiting'){
-          Waiting += 1
-        }else if(status.ReservationStatus == 'Rejected'){
-          Rejected += 1
-        }else if(status.ReservationStatus == 'Reserved'){
-          Reserved += 1
-        }
-        today_data = {date:DayToJPFromDateObj(today_JST), unread:Unread, cancelled:Cancelled, waiting:Waiting, rejected:Rejected, reserved:Reserved}
-      }
-    }else{
-      today_data = {date:MonthDayToJPFromDateObj(today_JST),day:WeekDayToJPFromDateObj(today_JST), unread:0, cancelled:0, waiting:0, rejected:0, reserved:0}
-    }
-    let tomorrowStatus = await psgl.ReservationStatusTomorrowByNursery(nurseryid)
-    if(tomorrowStatus.length > 0){
-      for (const status of await psgl.ReservationStatusTomorrowByNursery(nurseryid)) {
-        let Unread = 0
-        let Cancelled = 0
-        let Waiting = 0
-        let Rejected = 0
-        let Reserved = 0
-        if(status.ReservationStatus == 'Unread'){
-          Unread += 1
-        }else if(status.ReservationStatus == 'Cancelled'){
-          Cancelled += 1
-        }else if(status.ReservationStatus == 'Waiting'){
-          Waiting += 1
-        }else if(status.ReservationStatus == 'Rejected'){
-          Rejected += 1
-        }else if(status.ReservationStatus == 'Reserved'){
-          Reserved += 1
-        }
-        tomorrow_data = {date:DayToJPFromDateObj(tomorrow_JST), unread:Unread, cancelled:Cancelled, waiting:Waiting, rejected:Rejected, reserved:Reserved}
-      }
-    }else{
-      tomorrow_data = {date:MonthDayToJPFromDateObj(tomorrow_JST),day:WeekDayToJPFromDateObj(tomorrow_JST), unread:0, cancelled:0, waiting:0, rejected:0, reserved:0}
-    }
-    let dayaftertomorrowStatus = await psgl.ReservationStatusDayAfterTomorrowByNursery(nurseryid)
-    if(dayaftertomorrowStatus.length > 0){
-      for (const status of await psgl.ReservationStatusDayAfterTomorrowByNursery(nurseryid)) {
-        let Unread = 0
-        let Cancelled = 0
-        let Waiting = 0
-        let Rejected = 0
-        let Reserved = 0
-        if(status.ReservationStatus == 'Unread'){
-          Unread += 1
-        }else if(status.ReservationStatus == 'Cancelled'){
-          Cancelled += 1
-        }else if(status.ReservationStatus == 'Waiting'){
-          Waiting += 1
-        }else if(status.ReservationStatus == 'Rejected'){
-          Rejected += 1
-        }else if(status.ReservationStatus == 'Reserved'){
-          Reserved += 1
-        }
-        dayaftertomorrow_data = {date:DayToJPFromDateObj(dayaftertomorrow_JST), unread:Unread, cancelled:Cancelled, waiting:Waiting, rejected:Rejected, reserved:Reserved}
-      }
-    }else{
-      dayaftertomorrow_data = {date:MonthDayToJPFromDateObj(dayaftertomorrow_JST),day:WeekDayToJPFromDateObj(dayaftertomorrow_JST), unread:0, cancelled:0, waiting:0, rejected:0, reserved:0}
-    }
     status3days.push({id:nurseryid, name:nursery_name[0].NurseryName, today:today_data, tomorrow:tomorrow_data, dayaftertomorrow:dayaftertomorrow_data})
-    
     
     /*　予約確定・キャンセル待ち */
     let day1_reserved = []
