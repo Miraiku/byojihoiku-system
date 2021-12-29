b49097feefe7eb4d5342b347701d113e83897327
MinamiYODA <me@usaribbon.net>
2021-12-29 22:51:13 +0900

    ajax

--- public/js/functions.js
+++ public/js/functions.js
@@ -130,74 +130,77 @@ $(function() {
   });
 
   //update from /member/entry
+  
   $(".btn_update_member").on('click', function(e) {
-    const miraikuid = $('input[name="miraikuid"]').val()
-    const name = $('input[name="name"]').val()
-    const year = $('input[name="year"]').val()
-    const month = $('[name="month"] option:selected').val()
-    const day = $('[name="day"] option:selected').val()
-    const allergy = $('input[name="allergy"]:checked').val()
-    console.log(miraikuid)
-    console.log(name)
-    console.log(year)
-    console.log(month)
-    console.log(day)
-    console.log(allergy)
-    /*           
-    e.preventDefault(); 
-    $.confirmModal('「'+$(this).text()+'」でよろしいですか？', function(el) {
-      $.ajax({
-        url: '/updater',
-        type: 'POST',
-        data: {
-          'action': 'update_status_from_rsv',
-          'status': status,
-          'nurseryid': nurseryid,
-          'rsvid': rsvid
-        },
-        dataType: 'text'
-      }).done(function( data, textStatus, jqXHR ) {
-        notif({
-          type: "success",
-          position: "center",
-          autohide: true,
-          msg: "変更が完了しました",
-          opacity:0.8,
-          multiline: 0,
-          fade: 0,
-          bgcolor: "",
-          color: "",
-          timeout: 5000,
-          zindex: null,
-          offset: 0,
-          animation: 'slide'
-        });
-      }).fail(function( jqXHR, textStatus, errorThrown) {
-        let errmsg = ''
-        if(errorThrown == 'Service Unavailable'){
-          errmsg = '申し訳ありません、変更できませんでした'
-        }else if(errorThrown == 'Not Acceptable'){
-          errmsg = '変更先が満員のため変更できませんでした'
-        }
-        notif({
-          type: "error",
-          position: "center",
-          msg: errmsg,
-          opacity: 0.8,
-          multiline: 0,
-          fade: 0,
-          bgcolor: "",
-          color: "",
-          timeout: 5000,
-          zindex: null,
-          offset: 0,
-          animation: 'slide'
-        });
-        console.log("失敗"+errorThrown)
-      }).always(function( jqXHR, textStatus) {
-      });//end of ajax
-    })//end of confirm
-    */
+    if (memberInfo.validate().form()) {
+      const miraikuid = $('input[name="miraikuid"]').val()
+      const name = $('input[name="name"]').val()
+      const year = $('input[name="year"]').val()
+      const month = $('[name="month"] option:selected').val()
+      const day = $('[name="day"] option:selected').val()
+      const allergy = $('input[name="allergy"]:checked').val()
+      console.log(miraikuid)
+      console.log(name)
+      console.log(year)
+      console.log(month)
+      console.log(day)
+      console.log(allergy)         
+      e.preventDefault(); 
+      $.confirmModal('内容を変更しますか？', function(el) {
+        /*$.ajax({
+          url: '/updater',
+          type: 'POST',
+          data: {
+            'action': 'update_status_from_rsv',
+            'status': status,
+            'nurseryid': nurseryid,
+            'rsvid': rsvid
+          },
+          dataType: 'text'
+        }).done(function( data, textStatus, jqXHR ) {
+          notif({
+            type: "success",
+            position: "center",
+            autohide: true,
+            msg: "変更が完了しました",
+            opacity:0.8,
+            multiline: 0,
+            fade: 0,
+            bgcolor: "",
+            color: "",
+            timeout: 5000,
+            zindex: null,
+            offset: 0,
+            animation: 'slide'
+          });
+        }).fail(function( jqXHR, textStatus, errorThrown) {
+          let errmsg = ''
+          if(errorThrown == 'Service Unavailable'){
+            errmsg = '申し訳ありません、変更できませんでした'
+          }else if(errorThrown == 'Not Acceptable'){
+            errmsg = '変更先が満員のため変更できませんでした'
+          }
+          notif({
+            type: "error",
+            position: "center",
+            msg: errmsg,
+            opacity: 0.8,
+            multiline: 0,
+            fade: 0,
+            bgcolor: "",
+            color: "",
+            timeout: 5000,
+            zindex: null,
+            offset: 0,
+            animation: 'slide'
+          });
+          console.log("失敗"+errorThrown)
+        }).always(function( jqXHR, textStatus) {
+        });//end of ajax*/
+      })//end of confirm
+    } else {
+        return false
+    }
   });
 
   //update from /reservation/entry
