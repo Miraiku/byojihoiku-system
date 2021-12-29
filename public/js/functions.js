7e74366005290a8a2786323cb53c6c3dd90254a7
MinamiYODA <me@usaribbon.net>
2021-12-29 17:29:22 +0900

    confirm btn

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,10 +1,20 @@
-const message = "変更を保存しますか？";
-const title = "確認メッセージ";
 
-$(".btn_status_update").on('click', function(e) {
-  e.preventDefault();              
-  $.confirmModal('Are you sure you want to do this?', function(el) {
-    alert('OK was clicked!');
-    //do something...
-  });         
-});
+
+$(function() {
+  $defaultsConfirmModal = {
+    confirmButton: 'はい',
+    cancelButton: 'いいえ',
+    fadeAnimation: true,
+    modalVerticalCenter: true,
+    autoFocusOnConfirmBtn: true
+  };
+
+  $(".btn_status_update").on('click', function(e) {
+    e.preventDefault();              
+    $.confirmModal('変更してよろしいですか？', function(el) {
+      alert('OK was clicked!');
+      //do something...
+    });         
+  });
+
+});//end of jquery
