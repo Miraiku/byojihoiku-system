f1ef0cd3a31a896359ddfa9b054ed57909ff5979
MinamiYODA <me@usaribbon.net>
2021-12-31 16:44:28 +0900

    rsv status

--- public/js/functions.js
+++ public/js/functions.js
@@ -71,8 +71,8 @@ $(function() {
   //update from /reservation
   $(".btn_disable_member").on('click', function(e) {
     const del_memberid_name = $(this).val()
-    const memberid = btn_value.substr(0, del_memberid_name.indexOf('_'))
-    const name = btn_value.substr(del_memberid_name.indexOf('_') + 1)
+    const memberid = del_memberid_name.substr(0, del_memberid_name.indexOf('_'))
+    const name = del_memberid_name.substr(del_memberid_name.indexOf('_') + 1)
     e.preventDefault();            
     $.confirmModal(`${name}さんの情報を削除しますか？`, function(el) {
       $.ajax({
