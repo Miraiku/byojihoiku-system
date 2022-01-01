953bc0925f859cf44515f381ae747af3462f3938
MinamiYODA <me@usaribbon.net>
2022-01-01 15:57:58 +0900

    line prev action

--- index.js
+++ index.js
@@ -182,7 +182,9 @@ cron.schedule('0 0 7 * * *', async () => {
 });
 
 //前日リマインダー送信
-cron.schedule('0 0 20 * * *', async () => {
+
+cron.schedule('*/1  * * * *', async () =>  {
+//cron.schedule('0 0 20 * * *', async () => {
   try {
     let ids = await psgl.getLINEIDByReservedTomorrow()
     for (const id of ids) {
