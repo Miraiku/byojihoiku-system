3b37a18e90551d43f443f02e6ee1d5a06e2ecf96
MinamiYODA <me@usaribbon.net>
2022-01-05 13:47:29 +0900

    debug

--- modules/receiver_ajax.js
+++ modules/receiver_ajax.js
@@ -44,15 +44,41 @@ router
           res.status(406).send();
         }
       }else if(action == 'update_member_from_reservation_entry'){
-        let reservation_date = await psgl.getReservationDateByID(req.body.rsvid)
-        reservation_date = reservation_date[0].ReservationDate
-        reservation_date_formatted_psgl = view.getPsglTimeStampYMDFromDayDataObj(reservation_date)
-        let intime = reservation_date_formatted_psgl + ' ' + ('00' + req.body.intime_hour).slice(-2) + ':' + ('00' + req.body.intime_mins).slice(-2) + ':00'
-        let outtime = reservation_date_formatted_psgl + ' ' + ('00' + req.body.outtime_hour).slice(-2) + ':' + ('00' + req.body.outtime_mins).slice(-2) + ':00'
-        let updated = await psgl.updateReservationInfo(req.body, intime, outtime)
+        const status = req.body.status
+        const new_nurseryid = req.body.nurseryid
+        const rsvid = req.body.rsvid
+        let canUpdate = false
 
-        if(updated > 0 && updated != null){
-          res.status(200).send('Success');
+        let current_nurseryid = await psgl.getNurseryIDByResevationID(rsvid)
+        current_nurseryid = current_nurseryid[0].NurseryID
+        if(new_nurseryid != current_nurseryid){
+          let nursery_capacity = await psgl.getNurseryCapacityByID(new_nurseryid)
+          nursery_capacity = nursery_capacity[0].Capacity
+          let reservation_date = await psgl.getReservationDateByID(rsvid)
+          reservation_date = reservation_date[0].ReservationDate
+          let reservation_num_on_day = await psgl.canNurseryReservationOnThatDay(view.getPsglTimeStampFromDayDataObj(reservation_date), new_nurseryid)
+          let new_capacity = Number(reservation_num_on_day[0].count)
+          if((nursery_capacity - new_capacity) > 0){
+            canUpdate = true
+          }else{
+            canUpdate = false
+          }
+        }else{
+          canUpdate = true
+        }
+        if(canUpdate){
+          let reservation_date = await psgl.getReservationDateByID(req.body.rsvid)
+          reservation_date = reservation_date[0].ReservationDate
+          reservation_date_formatted_psgl = view.getPsglTimeStampYMDFromDayDataObj(reservation_date)
+          let intime = reservation_date_formatted_psgl + ' ' + ('00' + req.body.intime_hour).slice(-2) + ':' + ('00' + req.body.intime_mins).slice(-2) + ':00'
+          let outtime = reservation_date_formatted_psgl + ' ' + ('00' + req.body.outtime_hour).slice(-2) + ':' + ('00' + req.body.outtime_mins).slice(-2) + ':00'
+          let updated = await psgl.updateReservationInfo(req.body, intime, outtime)
+  
+          if(updated > 0 && updated != null){
+            res.status(200).send('Success');
+          }else{
+            res.status(503).send();
+          }
         }else{
           res.status(406).send();
         }
