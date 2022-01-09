d5ac0d463884c7ca076221b3632b8cb06c921875
MinamiYODA <me@usaribbon.net>
2022-01-09 22:05:43 +0900

    css

--- modules/view_data_render.js
+++ modules/view_data_render.js
@@ -264,7 +264,7 @@ exports.getMembersPage = async function (req, res){
       i = 1
     }
     let year10 = []
-    for (i = 1; i <= 11; i++) {
+    for (i = 1; i <= 12; i++) {
       const y = String(today.getFullYear()-i).toString().substr(-2)
       let result = await psgl.getYearMembersOrderByName(y)
       let mem = []
