8df62dfef789629a0c09fa1c3b5b04df597ecc82
MinamiYODA <me@usaribbon.net>
2022-02-10 16:15:23 +0900

    bug fix rsv

--- modules/view_data_render.js
+++ modules/view_data_render.js
@@ -778,17 +778,12 @@ exports.getReservationEntryPage = async function (req, res){
     let bros_num = 0
     if(sameday_members.length > 0){
       for (const m of sameday_members) {
-        console.log(`sameday_members ${sameday_members.length}`)
         let mem = await psgl.getLINEIDByMemberID(m.MemberID)
-        console.log(`lineid[0].LINEID ${lineid[0].LINEID}`)
-        console.log(`mem[0].LINEID ${mem[0].LINEID}`)
-        console.log(`mem.length ${mem.length}`)
         if(mem.length > 0 && mem[0].LINEID == lineid[0].LINEID){
           bros_num += 1
         }
       }
     }
-    console.log(`bros_num ${bros_num}`)
     if(bros_num > 1){
       brothers = '有り'
     }else{
